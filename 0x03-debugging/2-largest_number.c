ee5487324463cbd991bddccf0e41b3f40e6fea2c
Shina Salau <114442903+doubleyoushinamide@users.noreply.github.com>
2023-02-23 12:43:48 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -9,21 +9,20 @@
  */
 
 int largest_number(int a, int b, int c)
+{int largest;
+
+if (a > b && a > c)
 {
-    int largest;
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+return (largest);
 }
