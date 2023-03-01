aef8b7a20d99063b3af6a6c41735093e5927035b
Shina Salau <114442903+doubleyoushinamide@users.noreply.github.com>
2023-03-01 21:26:59 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,22 +9,7 @@
  */
 void print_number(int n)
 {
-	unsigned int u, i;
-
-	if (n < 0)
-	{
-		u = -n;
-		_putchar('-');
-	}
-	else
-	{
-		u = n;
-	}
-
-	i = 1000000000;
-	do {
-		if (i <= u || i == 1)
-			_putchar(u / i % 10 + '0');
-		i /= 10;
-	} while (i != 0);
+	unsigned int u, i
+	_putchar((n/10) % 10 + '0');
+	_putchar(n % 10 + '0');
 }
