8be2e41f37aeb2543aec80612c2bf6c30975ccab
Shina Salau <114442903+doubleyoushinamide@users.noreply.github.com>
2023-03-01 21:11:45 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,47 @@
-#include "main.h"
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+    // initialise result array 
+    int res[size_r];
 
-/**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
- */
+    int n1_len = strlen(n1);
+    int n2_len = strlen(n2);
+  
+    // storing the last digit of number 1 in a variable 
+    int carry = 0;
 
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
+    // Initializing indexes of result and numbers to be added 
+    int i, j, k = 0; 
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+    // Traverse from end of both strings 
+    while (i >= 0 || j >= 0 || carry) { 
+        // computing sum of digits and carry 
+        int x = carry; 
 
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
+        if (i >= 0) {
+            x += n1[i] - '0'; 
+        }
+        
+        if (j >= 0) {
+            x += n2[j] - '0'; 
+        }
 
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
+        // updating result array and decrementing indices 
+        res[k++] = (x % 10); 
+        carry = x / 10; 
+ 
+        i --; 
+        j --;     
+    } 
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
-
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
-	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
-}
+    // reverse the result array 
+    reverse(res, k); 
+    
+    // Convert the result into string 
+    // bring the result into the buffer 
+    int index = 0; 
+    while (index < k && index < size_r - 1) { 
+        r[index] = (res[index] + '0'); 
+        index ++; 
+    } 
+    r[index] = '\0'; 
+    return r;
+} 
