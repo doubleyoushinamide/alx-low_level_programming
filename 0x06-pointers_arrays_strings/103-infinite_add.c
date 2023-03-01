a015d1c9e1fd640424f00e824790748f6e94e6fe
Shina Salau <114442903+doubleyoushinamide@users.noreply.github.com>
2023-03-01 21:19:00 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,55 @@
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-    // initialise result array 
+#include "main.h"
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
     int res[size_r];
 
     int n1_len = strlen(n1);
     int n2_len = strlen(n2);
-  
-    // storing the last digit of number 1 in a variable 
+
     int carry = 0;
 
-    // Initializing indexes of result and numbers to be added 
-    int i, j, k = 0; 
+    int i, j, k = 0;
 
-    // Traverse from end of both strings 
-    while (i >= 0 || j >= 0 || carry) { 
-        // computing sum of digits and carry 
-        int x = carry; 
+   while (i >= 0 || j >= 0 || carry)
+   {
+        int x = carry;
 
-        if (i >= 0) {
-            x += n1[i] - '0'; 
+        if (i >= 0)
+        {
+            x += n1[i] - '0';
         }
         
-        if (j >= 0) {
-            x += n2[j] - '0'; 
+        if (j >= 0)
+        {
+            x += n2[j] - '0';
         }
 
-        // updating result array and decrementing indices 
-        res[k++] = (x % 10); 
-        carry = x / 10; 
- 
-        i --; 
-        j --;     
-    } 
 
-    // reverse the result array 
-    reverse(res, k); 
-    
-    // Convert the result into string 
-    // bring the result into the buffer 
-    int index = 0; 
-    while (index < k && index < size_r - 1) { 
-        r[index] = (res[index] + '0'); 
-        index ++; 
-    } 
-    r[index] = '\0'; 
+        res[k++] = (x % 10);
+        carry = x / 10;
+ 
+        i --;
+        j --;
+    }
+
+    reverse(res, k);
+
+    int index = 0;
+    while (index < k && index < size_r - 1)
+    {
+        r[index] = (res[index] + '0');
+        index ++;
+    }
+    r[index] = '\0';
     return r;
-} 
+}
