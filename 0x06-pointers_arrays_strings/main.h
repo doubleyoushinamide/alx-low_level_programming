0cee37086ac1771f944fd2871b9b0072ffaff236
Shina Salau <114442903+doubleyoushinamide@users.noreply.github.com>
2023-03-01 20:52:40 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Description: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory
+ */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -12,3 +20,5 @@ void _putchar(char);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
