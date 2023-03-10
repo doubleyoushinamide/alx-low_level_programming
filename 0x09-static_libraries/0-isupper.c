3cfdc37e2e711928d8f7d64a787b0f70d94051cc
doubleyoushinamide <olusholasalau@gmail.com>
2023-03-10 07:28:46 -0800

    Created libraries, achived them and done!

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - contains arg.
+ * _isupper: converts to uppercas
+ * @c: parameter
+ * Return: returns 0 
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,5 @@
+# C - Static libraries
+
+* useful tools:
+* editor ``` vim ```
+* Program: ``` ALX Schools Cohort 12 ```
