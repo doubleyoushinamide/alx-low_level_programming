927455d51ab2d00142a2bc7939aacbe3464b7942
doubleyoushinamide <olusholasalau@gmail.com>
2023-04-05 05:46:03 -0700

    fixed and removed the initial loop

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,14 +8,21 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *ptr = malloc(sizeof(listint_t));
 
-	if (new_node == NULL)
-		return NULL;
+	if (ptr == NULL)
+		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
+	ptr->n = n;
+	ptr->next = NULL;
 
-	return new_node;
+	if (*head == NULL)
+	{
+		*head = ptr;
+		return (*head);
+	}
+
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
 }
